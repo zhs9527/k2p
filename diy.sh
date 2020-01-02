9c9098cb6d20d862e92ab4de96fb796b1fa6fe1e
au3300 <45656560+au3300@users.noreply.github.com>
2020-01-02 07:36:30 -0600

    K2P编译配置文件

---
+++ diy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#=================================================
+#   Description: DIY script
+#   Lisence: MIT
+#   Author: P3TERX
+#   Blog: https://p3terx.com
+#=================================================
